07dcd2120ea7dedc93c5f2990eb3287fcf3a95e2
tatyana <tilkka@mail.ru>
2024-10-10 23:34:50 +0300

    refactor: change code (DRY)

--- 03-components/20-UiClock/UiClock.js
+++ 03-components/20-UiClock/UiClock.js
@@ -4,12 +4,13 @@ export default defineComponent({
   name: 'UiClock',
 
   setup() {
-    const id = ref(0)
-    const time = ref(new Date().toLocaleTimeString(navigator.language, { timeStyle: 'medium' }))
-
     const getTime = () => {
       return new Date().toLocaleTimeString(navigator.language, { timeStyle: 'medium' })
     }
+
+    const id = ref(0)
+    const time = ref(getTime())
+
     const startTime = () => {
       id.value = setInterval(() => {
         time.value = getTime()
