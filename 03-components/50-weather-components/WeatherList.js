b893d2f55ec853a302d54d88be9f16fb15317f87
tatyana <tilkka@mail.ru>
2024-10-12 23:59:33 +0300

    refactor: add separate component for weather list item

--- 03-components/50-weather-components/WeatherList.js
+++ 03-components/50-weather-components/WeatherList.js
@@ -1,7 +1,12 @@
 import { defineComponent } from "vue";
+import WeatherListItem from "./WeatherListItem";
 
 export default defineComponent({
   name: 'WeatherList',
+
+  components: {
+    WeatherListItem
+  },
 
   props: {
     weatherData: {
@@ -30,40 +35,7 @@ export default defineComponent({
         :key="weather.geographic_name"
         class="weather-card"
         :class="{'weather-card--night': !isDayTime(weather.current.dt, weather.current.sunrise, weather.current.sunset)}">
-      <div v-if="weather.alert" class="weather-alert">
-        <span class="weather-alert__icon">⚠️</span>
-        <span class="weather-alert__description">Королевская метеослужба короля Арагорна II: Предвещается наступление сильного шторма.</span>
-      </div>
-      <div>
-        <h2 class="weather-card__name">
-          {{ weather.geographic_name }}
-        </h2>
-        <div class="weather-card__time">
-          {{ weather.current.dt }}
-        </div>
-      </div>
-        <div class="weather-conditions">
-          <div class="weather-conditions__icon" :title="weather.current.weather.description">{{ weatherConditionIcons[weather.current.weather.id] }}</div>
-        <div class="weather-conditions__temp">{{ (weather.current.temp - 273.15).toFixed(1) }} °C</div>
-      </div>
-      <div class="weather-details">
-        <div class="weather-details__item">
-          <div class="weather-details__item-label">Давление, мм рт. ст.</div>
-          <div class="weather-details__item-value">{{ Math.round(weather.current.pressure * 0.75) }}</div>
-        </div>
-        <div class="weather-details__item">
-          <div class="weather-details__item-label">Влажность, %</div>
-          <div class="weather-details__item-value">{{ weather.current.humidity }}</div>
-        </div>
-        <div class="weather-details__item">
-          <div class="weather-details__item-label">Облачность, %</div>
-          <div class="weather-details__item-value">{{ weather.current.clouds }}</div>
-        </div>
-        <div class="weather-details__item">
-          <div class="weather-details__item-label">Ветер, м/с</div>
-          <div class="weather-details__item-value">{{ weather.current.wind_speed }}</div>
-      </div>
-    </div>
+      <WeatherListItem :weather :icon="weatherConditionIcons[weather.current.weather.id]"/>
     </li>
 </ul>
 `
